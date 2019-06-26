69b584d494e6011ec1ad4dcf09dbdae70c2a53c1
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2019-06-26 14:33:17 +0200

    Added V1.4.
    First commit.

---
+++ script.bat
@@ -0,0 +1,23 @@
+@echo off
+REM if you are reading this, let's have some fun, don't spoil it!
+REM seriously, come on
+REM PLEASE update and make it seem complicated if you want, I have to go soon
+title Steam Summer Sale 2019 Hack
+echo Starting RabidTokens v1.4
+echo WARNING : This program is in no way affiliated with Valve or Steam!
+echo Make sure you are logging into Steam before continuing...
+echo USE AT YOUR OWN RISK!
+pause
+set /A count=1
+:loop
+echo Adding tokens : %count%
+set /A count=count+1
+IF %count% == 25000 (
+	goto end
+)
+goto loop
+:end
+echo ERROR : REQUEST BLOCKED BY VALVE!
+echo ERROR : YOUR STEAM ACCOUNT HAS BEEN BANNED!
+goto end
+
